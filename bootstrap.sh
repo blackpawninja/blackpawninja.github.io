0cb70ecb2ba347a9aa8e58d828fe2d20d4d573a3
Khucink Ithem <meongx@gmail.com>
2018-09-13 17:00:04 +0800

    disable ruby docs on install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+echo "gem: --no-document" > ~/.gemrc
+
 sudo apt-add-repository ppa:brightbox/ruby-ng --yes
 sudo apt-get update
 sudo apt-get install build-essential zlib1g-dev ruby2.5 ruby2.5-dev --yes
